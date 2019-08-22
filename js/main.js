c097735db9cb682c5d34c0174ab2fb1c457e87cc
sal <raihan@escenic.com>
2019-08-22 15:27:30 +0600

    Put all JS codes inside IIFE to avoid global scope.

--- js/main.js
+++ js/main.js
@@ -1,93 +1,93 @@
-const API_ENDPOINT_GITHUB = 'https://api.github.com/users/rizvi322';
+(() => {
+  const API_ENDPOINT_GITHUB = 'https://api.github.com/users/rizvi322';
 
-const containerDiv = document.querySelector('#container');
-let uiScriptLoaded = false;
+  const containerDiv = document.querySelector('#container');
+  let uiScriptLoaded = false;
 
-const initLSCache = () => {
-  const expirationKey = 'lastExpirationDate';
-  const expirationDate = lscache.get(expirationKey);
-  const currentDate = new Date();
+  const initLSCache = () => {
+    const expirationKey = 'lastExpirationDate';
+    const expirationDate = lscache.get(expirationKey);
+    const currentDate = new Date();
 
-  if (!expirationDate) {
-    lscache.set(expirationKey, currentDate);
-  } else if (new Date(expirationDate).getDate() !== currentDate.getDate()) {
-    lscache.flush();
-    lscache.set(expirationKey, currentDate);
-  }
-};
+    if (!expirationDate) {
+      lscache.set(expirationKey, currentDate);
+    } else if (new Date(expirationDate).getDate() !== currentDate.getDate()) {
+      lscache.flush();
+      lscache.set(expirationKey, currentDate);
+    }
+  };
 
-const renderTemplate = (template, context = {}) => {
-  twig({
-    id: template,
-    href: `templates/${template}.twig`,
-    load: twig => {
-      containerDiv.innerHTML = twig.render(context);
+  const renderTemplate = (template, context = {}) => {
+    twig({
+      id: template,
+      href: `templates/${template}.twig`,
+      load: twig => {
+        containerDiv.innerHTML = twig.render(context);
 
-      if(!uiScriptLoaded) {
-        uiScript = document.createElement('script');
-        uiScript.src = 'js/ui.js';
+        if (!uiScriptLoaded) {
+          uiScript = document.createElement('script');
+          uiScript.src = 'js/ui.js';
 
-        document.body.appendChild(uiScript);
-        uiScriptLoaded = true;
-      }
-    },
-    namespace: 'templates/'
-  });
-};
-
-const fetchMyGithubProfileData = async () => {
-  const cacheKey = 'githubData';
-  let githubData = lscache.get(cacheKey);
-
-  if (githubData) {
-    return githubData;
-  }
-
-  githubData = await fetch(API_ENDPOINT_GITHUB).then(
-    async result => await result.json()
-  );
-  lscache.set(cacheKey, githubData);
-  return githubData;
-};
-
-fetchAllRequiredData = async () => {
-  const data = {};
-
-  await fetch('../assets/data.json')
-    .then(async result => await result.json())
-    .then(json => {
-      data['local'] = json;
-    });
-
-  await fetchMyGithubProfileData().then(githubData => {
-    data['github'] = githubData;
-  });
-
-  return data;
-};
-
-const loadIndexTemplate = () => {
-  fetchAllRequiredData().then(data => {
-    const githubData = data.github;
-    const localData = data.local;
-
-    renderTemplate('index', {
-      name: githubData.name,
-      avatarUrl: githubData.avatar_url,
-      company: {
-        name: localData.experience[0].name,
-        designation: localData.experience[0].designation,
-        url: localData.experience[0].url
+          document.body.appendChild(uiScript);
+          uiScriptLoaded = true;
+        }
       },
-      bio: githubData.bio,
-      education: localData.education,
-      experience: localData.experience,
-      aboutMe: localData.aboutMe
+      namespace: 'templates/'
     });
-  });
-};
+  };
 
-initLSCache();
-loadIndexTemplate();
+  const fetchMyGithubProfileData = async () => {
+    const cacheKey = 'githubData';
+    let githubData = lscache.get(cacheKey);
 
+    if (githubData) {
+      return githubData;
+    }
 
+    githubData = await fetch(API_ENDPOINT_GITHUB).then(
+      async result => await result.json()
+    );
+    lscache.set(cacheKey, githubData);
+    return githubData;
+  };
+
+  fetchAllRequiredData = async () => {
+    const data = {};
+
+    await fetch('../assets/data.json')
+      .then(async result => await result.json())
+      .then(json => {
+        data['local'] = json;
+      });
+
+    await fetchMyGithubProfileData().then(githubData => {
+      data['github'] = githubData;
+    });
+
+    return data;
+  };
+
+  const loadIndexTemplate = () => {
+    fetchAllRequiredData().then(data => {
+      const githubData = data.github;
+      const localData = data.local;
+
+      renderTemplate('index', {
+        name: githubData.name,
+        avatarUrl: githubData.avatar_url,
+        company: {
+          name: localData.experience[0].name,
+          designation: localData.experience[0].designation,
+          url: localData.experience[0].url
+        },
+        bio: githubData.bio,
+        education: localData.education,
+        experience: localData.experience,
+        aboutMe: localData.aboutMe
+      });
+    });
+  };
+
+  initLSCache();
+  loadIndexTemplate();
+})();
--- js/ui.js
+++ js/ui.js
@@ -1,116 +1,118 @@
-// Markdown experience details codes
-const mdConverter = new showdown.Converter();
+(() => {
+  // Markdown experience details codes
+  const mdConverter = new showdown.Converter();
 
-const loadMarkdownFileContent = async (fileName, container) => {
-  const markdownText = await fetch('../assets/markdowns/' + fileName).then(
-    async result => await result.text()
+  const loadMarkdownFileContent = async (fileName, container) => {
+    const markdownText = await fetch('../assets/markdowns/' + fileName).then(
+      async result => await result.text()
+    );
+
+    if (markdownText) {
+      container.innerHTML = mdConverter.makeHtml(markdownText);
+    }
+  };
+
+  const companies = document.querySelectorAll('.company');
+
+  companies.forEach(company => {
+    const data = JSON.parse(company.dataset['company']);
+    const responsibilityContainer = company.querySelector('.responsibility');
+
+    if (data.expFile) {
+      loadMarkdownFileContent(data.expFile, responsibilityContainer);
+    } else if (data.responsibility) {
+      responsibilityContainer.innerHTML += data.responsibility;
+    }
+  });
+  // Ends Markdown experience details codes
+
+  // Scroll Magic codes
+  const controller = new ScrollMagic.Controller();
+
+  new ScrollMagic.Scene({
+    duration: `${(companies.length - 1) * 100}%`,
+    triggerElement: '.experience-heading',
+    triggerHook: 0
+  })
+    .setPin('.experience-heading')
+    .addTo(controller);
+
+  // Ends Scroll Magic codes
+
+  // Intersection objservver codes
+  const intersectionOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.0
+  };
+
+  const intersectionCallback = (entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.querySelectorAll('.track-child').forEach(child => {
+          child.classList.add('animate');
+        });
+        observer.unobserve(entry.target);
+      }
+    });
+  };
+
+  const observer = new IntersectionObserver(
+    intersectionCallback,
+    intersectionOptions
   );
 
-  if (markdownText) {
-    container.innerHTML = mdConverter.makeHtml(markdownText);
-  }
-};
+  document
+    .querySelectorAll('.track')
+    .forEach(element => observer.observe(element));
+  // Ends Intersection objservver codes
 
-const companies = document.querySelectorAll('.company');
+  // Site menu codes
+  const siteMenu = document.querySelector('.site-menu');
+  const hamberger = document.querySelector('.hamburger');
+  const hambergerLink = document.querySelector('.hamburger-link');
 
-companies.forEach(company => {
-  const data = JSON.parse(company.dataset['company']);
-  const responsibilityContainer = company.querySelector('.responsibility');
-
-  if (data.expFile) {
-    loadMarkdownFileContent(data.expFile, responsibilityContainer);
-  } else if (data.responsibility) {
-    responsibilityContainer.innerHTML += data.responsibility;
-  }
-});
-// Ends Markdown experience details codes
-
-// Scroll Magic codes
-const controller = new ScrollMagic.Controller();
-
-new ScrollMagic.Scene({
-  duration: `${(companies.length - 1) * 100}%`,
-  triggerElement: '.experience-heading',
-  triggerHook: 0
-})
-  .setPin('.experience-heading')
-  .addTo(controller);
-
-// Ends Scroll Magic codes
-
-// Intersection objservver codes
-const intersectionOptions = {
-  root: null,
-  rootMargin: '0px',
-  threshold: 0.0
-};
-
-const intersectionCallback = (entries, observer) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.querySelectorAll('.track-child').forEach(child => {
-        child.classList.add('animate');
-      });
-      observer.unobserve(entry.target);
-    }
-  });
-};
-
-const observer = new IntersectionObserver(
-  intersectionCallback,
-  intersectionOptions
-);
-
-document
-  .querySelectorAll('.track')
-  .forEach(element => observer.observe(element));
-// Ends Intersection objservver codes
-
-// Site menu codes
-const siteMenu = document.querySelector('.site-menu');
-const hamberger = document.querySelector('.hamburger');
-const hambergerLink = document.querySelector('.hamburger-link');
-
-if (siteMenu && hambergerLink) {
-  hambergerLink.addEventListener('click', () => {
-    if (siteMenu.classList.contains('hide')) {
-      showSiteMenu();
-    } else {
-      hideSiteMenu();
-    }
-  });
-
-  const menuItemLinks = document.querySelectorAll('.menu-item-link');
-
-  menuItemLinks.forEach(menuItemLink => {
-    menuItemLink.addEventListener('click', event => {
-      const currentMenuItem = event.target;
-
-      if (!currentMenuItem.classList.contains('active')) {
-        removeActiveClassFromAllMenuItemLink(menuItemLinks);
-        currentMenuItem.classList.add('active');
+  if (siteMenu && hambergerLink) {
+    hambergerLink.addEventListener('click', () => {
+      if (siteMenu.classList.contains('hide')) {
+        showSiteMenu();
+      } else {
+        hideSiteMenu();
       }
-
-      hideSiteMenu();
     });
-  });
-}
 
-const removeActiveClassFromAllMenuItemLink = menuItemLinks => {
-  menuItemLinks.forEach(menuItemLink => {
-    menuItemLink.classList.remove('active');
-  });
-};
+    const menuItemLinks = document.querySelectorAll('.menu-item-link');
 
-const showSiteMenu = () => {
-  siteMenu.classList.remove('hide');
-  siteMenu.classList.add('show');
-  hamberger.classList.remove('closed');
-};
+    menuItemLinks.forEach(menuItemLink => {
+      menuItemLink.addEventListener('click', event => {
+        const currentMenuItem = event.target;
 
-const hideSiteMenu = () => {
-  siteMenu.classList.remove('show');
-  siteMenu.classList.add('hide');
-  hamberger.classList.add('closed');
-};
-// Ends site menu codes
+        if (!currentMenuItem.classList.contains('active')) {
+          removeActiveClassFromAllMenuItemLink(menuItemLinks);
+          currentMenuItem.classList.add('active');
+        }
+
+        hideSiteMenu();
+      });
+    });
+  }
+
+  const removeActiveClassFromAllMenuItemLink = menuItemLinks => {
+    menuItemLinks.forEach(menuItemLink => {
+      menuItemLink.classList.remove('active');
+    });
+  };
+
+  const showSiteMenu = () => {
+    siteMenu.classList.remove('hide');
+    siteMenu.classList.add('show');
+    hamberger.classList.remove('closed');
+  };
+
+  const hideSiteMenu = () => {
+    siteMenu.classList.remove('show');
+    siteMenu.classList.add('hide');
+    hamberger.classList.add('closed');
+  };
+  // Ends site menu codes
+})();
